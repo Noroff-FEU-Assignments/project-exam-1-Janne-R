99a598e818316ed44a84659f496104a16039be66
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-09-03 13:32:38 +0200

    js fix blogpost page

--- js/blogPost.js
+++ js/blogPost.js
@@ -1,3 +1,68 @@
+/*const blogPosts = document.querySelector(".blog-posts");
+const url = "https://familykitchen.janne-ringdal.one/wp-json/wp/v2/posts/?per_page=20";
+
+
+
+
+async function getPosts() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    console.log(json);
+
+    const posts = json[0];
+
+    posts.forEach(function (result) {
+      blogPosts.innerHTML +=
+        `
+        <h2>${result.title}</h2>
+      `;
+    })
+
+
+
+  } catch (error) {
+    blogPosts.innerHTML = "error";
+
+  }
+}
+
+
+getPosts();*/
+
+/*
+
+const blogPosts = document.querySelector(".blog-posts");
+const url = "https://familykitchen.janne-ringdal.one/wp-json/wp/v2/posts/?per_page=20";
+
+async function getPosts() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    console.log(json);
+    return json[0];
+
+
+  } catch (error) {
+    blogPosts.innerHTML = "error";
+
+  }
+}
+
+
+const results = getPosts();
+
+results.forEach(function (result) {
+  blogPosts.innerHTML +=
+    `
+      <h2>${result[0].title}</h2>
+ `;
+
+}
+);*/
+
 const blogPosts = document.querySelector(".blog-posts");
 const url = "https://familykitchen.janne-ringdal.one/wp-json/wp/v2/posts/?per_page=20";
 
@@ -8,7 +73,13 @@ async function getPosts() {
 
     console.log(json);
 
-    createHtml(json);
+    json.forEach(function (result) {
+      blogPosts.innerHTML += `
+   
+      <h2>${json[0].title.rendered}</h2>
+    `;
+
+    });
 
 
   } catch (error) {
@@ -20,11 +91,3 @@ async function getPosts() {
 
 getPosts();
 
-
-function createHtml(json) {
-  blogPosts.innerHTML +=
-    `
-    <h2>${json[0].title.rendered}</h2>
-  `;
-
-}
