573f886bb315765b8b34ffbc8ca53d65ba96bb46
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-09-13 15:26:37 +0200

    modal img vol 2

--- js/blogSpecific.js
+++ js/blogSpecific.js
@@ -31,44 +31,30 @@ function createHtml(details) {
   <h1>${details.title.rendered}</h1>
   <p>${details.excerpt.rendered}</p>
   
-  
-  <img id="myImg" src="${getImageUrl(details.featured_media, details._embedded["wp:featuredmedia"])}" alt="Snow" style="width:100%;max-width:300px">
-<div id="myModal" class="modal">
-
-  <button class="close"><i class="fas fa-times"></i></button>
-
-
-  <img class="modal-content" id="img01">
-
- 
-  <div id="caption"></div>
-</div>
-  
+  <img class="modalImg" src="${getImageUrl(details.featured_media, details._embedded["wp:featuredmedia"])}" alt="${details.title.rendered}" style="width:100%">
+  <div class="modal">
+    <button class="close"><i class="fas fa-times"></i></button>
+    <img class="modal-content img01">
+    <div class="caption"></div>
+  </div>
   <p>${details.content.rendered}</p>
 
   `;
 
 }
 
+var modal = document.querySelector(".modal");
+var img = document.querySelector(".modalImg");
+var modalImg = document.querySelector(".img01");
+var captionText = document.querySelector(".caption");
+var closeButton = document.querySelector(".close");
 
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
 img.onclick = function () {
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
-var closeButton = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
 closeButton.onclick = function () {
   modal.style.display = "none";
 }
